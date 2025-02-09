704a211f1a2406c03a2f0ab4d1bf2832082aabd0
bibek <bbhattarai770@gmail.com>
2025-02-09 15:43:07 +0545

    added staticfiles

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include  
 from boards import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
 
     path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
