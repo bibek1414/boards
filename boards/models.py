81687889c5e7f309b8962d955989cdbbb93f2980
bibek1414 <bbhattarai770@gmail.com>
2025-02-23 13:33:58 +0545

    added {{ post.get_message_as_markdown }} in reply and topic

--- boards/models.py
+++ boards/models.py
@@ -1,6 +1,8 @@
+
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils.html import mark_safe
+from markdown import markdown
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
@@ -16,7 +18,6 @@ class Board(models.Model):
         return Post.objects.filter(topic__board=self).order_by('-created_at').first()
 
 
-
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
@@ -27,13 +28,13 @@ class Topic(models.Model):
     def __str__(self):
         return self.subject
 
-
 class Post(models.Model):
     message = models.TextField(max_length=4000)
-    topic = models.ForeignKey(Topic, related_name='posts',on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User, related_name='posts',on_delete=models.CASCADE)
-    updated_by = models.ForeignKey(User, null=True, related_name='+',on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
 
-
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
